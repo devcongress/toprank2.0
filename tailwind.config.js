f1c809eb5d083a05742a6b443ba2971c985e8873
rasheedmhd <starletgh@gmail.com>
2024-01-29 11:59:02 +0000

    landing page animated gradients completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,13 +20,32 @@ module.exports = {
         cardTeal:     "#2CADF5",
         cardBeige:    "#FAEFD3",
         cardWebTeal:  "#29B3D2",
-        webTeal: "#00A3FF",
+        webTeal:      "#00A3FF",
         cardGreen:    "#B5FE93",
         textGreen:    "#14532d",
         cardPurple:   "#7000FF",
         typeGreen:    "#A6D229",
       },
     },
+    animation: {
+      blob: "blob 7s infinite",
+    },
+    keyframes: {
+      blob: {
+        "0%": {
+          transform: "translate(0px, 0px) scale(1)"
+        },
+        "33%": {
+          transform: "translate(30px, -50px) scale(1.1)"
+        },
+        "66%": {
+          transform: "translate(-20px, 20px) scale(0.9)"
+        },
+        "100%": {
+          transform: "translate(0px, 0px) scale(1)"
+        },
+      }
+    }
   },
   plugins: [],
 }
